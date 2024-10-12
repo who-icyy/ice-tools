07676dae34c1a52547885febb07ebf88c3720363
who-icyy <itscobuu@gmail.com>
2024-10-12 13:56:31 +0530

    Initial

--- main.py
+++ main.py
@@ -7,11 +7,21 @@ def main():
     
     # Add an argument for the name
     parser.add_argument('path', type=str, help='File Path')
-    parser.add_argument('-o',"--output", type=str, help='File Path', required=True,)
+    parser.add_argument('-t',"--tool", type=str, help='<Tool Name> mrkdown --> Convert Markdown to HTML Format', required=False,)
+    parser.add_argument('-o',"--output", type=str, help='Output Path', required=True,)
 
     
     # Parse the arguments
     args = parser.parse_args()
+
+    if args.tool == "m2h":
+        try:
+            mrkdownToHTML(args.path,args.output+".html")
+        except:
+            print("Unable to read your file.")
+    
+    else:
+        print("Please choose vaiid -t && --tool")
 
         
 
