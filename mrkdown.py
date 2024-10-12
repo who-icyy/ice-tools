a672be9c167be84675b6c16e82b80098cc854d19
who-icyy <itscobuu@gmail.com>
2024-10-12 14:44:20 +0530

    Initial

--- mrkdown.py
+++ mrkdown.py
@@ -1,3 +1,4 @@
+import argparse
 import markdown
 
 
@@ -8,4 +9,24 @@ def mrkdownToHTML(path:str,output:str):
     mrkdown.close()
     html = open(output,"w",encoding='latin1')
     html.write(results)
+
+def main():
+    # Create the parser
+    parser = argparse.ArgumentParser(description='A markdown to html converter')
+    
+    # Add an argument for the name
+    parser.add_argument('path', type=str, help='File Path')
+    parser.add_argument('-o',"--output", type=str, help='Output Path', required=True,)    
+    # Parse the arguments
+    args = parser.parse_args()
+
+    
+    try:
+        mrkdownToHTML(args.path,args.output+".html")
+    except:
+        print("Unable to read your file.")
+
+
+if __name__ == '__main__':
+    main()
 
